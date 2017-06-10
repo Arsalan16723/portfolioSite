6a3fccdd21ae079fffce1ea1f208c53d20b02d74
Arsalan <m.arsalan16723@gmail.com>
2017-06-10 20:48:21 +0000

    Added new and create actions for portfolio item along with show action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,29 @@
 class PortfoliosController < ApplicationController
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+		@portfolio_item = Portfolio.new(portfolio_params)
+		if @portfolio_item.save
+			flash[:notice] = 'item created'
+			redirect_to @portfolio_item
+		else
+			redirect root_path
+		end
+	end
+	def show
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
+
+
+
+
+private
+def portfolio_params
+params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+end
+
 end
