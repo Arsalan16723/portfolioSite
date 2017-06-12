f9b16df74efdf3f03c09a556901dd0de7110378f
Arsalan <m.arsalan16723@gmail.com>
2017-06-12 23:57:39 +0000

    ADDED Enum to blogs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
-	Rails.application.routes.draw do
-	  resources :portfolios
-	   get 'about', to: 'pages#about'
-	  get 'contact', to: 'pages#contact'
+Rails.application.routes.draw do
+  resources :portfolios
+   get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
-	  resources :blogs
-	  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	  get 'home', to: 'pages#home'
-	  root to: 'pages#home'
-	end
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home', to: 'pages#home'
+  root to: 'pages#home'
+end
